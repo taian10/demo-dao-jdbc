77347bd90d5e0d9ce9d749bbe0fb7f219e9fae9f
taian10 <taianmendes@hotmail.com>
2022-04-05 15:31:53 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-				
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
+		System.out.println("===== TESTE 1: seller findById =====");
 		Seller seller = sellerDao.findById(1);
-		
 		System.out.println(seller);
-		
 
 	}
 }
